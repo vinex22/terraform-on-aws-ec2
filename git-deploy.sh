7044e78f881b25322dede6e4b8d288c8b7f000c2
Vinay Jain <vinex22@gmail.com>
2022-03-04 17:12:35 +0800

    Welcome to vinex22 terraforming

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Add files and do local commit"
 git add .
-git commit -am "Welcome to StackSimplify"
+git commit -am "Welcome to vinex22 terraforming"
 
 echo "Pushing to Github Repository"
 git push
